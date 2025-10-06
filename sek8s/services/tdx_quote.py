514760dfe4a1b35b092b7943010c43a8393930ef
Kyle Widmann <kyle.widmann@gmail.com>
2025-10-06 16:07:45 -0400

    Add initial tdx quote service

---
+++ sek8s/services/tdx_quote.py
@@ -0,0 +1,75 @@
+from asyncio import subprocess
+import json
+from tempfile import TemporaryFile
+import tempfile
+from fastapi import HTTPException, Query, status
+import logging
+from loguru import logger
+from sek8s.config import ServerConfig
+from sek8s.server import WebServer
+
+QUOTE_GENERATOR_BINARY = "/usr/bin/tdx-quote-generator"
+
+
+class TdxQuoteServer(WebServer):
+    """Async web server for admission webhook."""
+
+    def _setup_routes(self):
+        """Setup web routes."""
+        self.app.router.add_get("/quote", self.get_quote)
+
+    def get_quote(nonce: str = Query(None, description="Nonce to include in the quote")):
+        try:
+            with tempfile.NamedTemporaryFile(mode="r", suffix=".bin") as fp:
+                result = subprocess.run(
+                    [QUOTE_GENERATOR_BINARY, "--user-data", nonce, "--output", fp.name],
+                    capture_output=True,
+                    check=True,
+                )
+
+                if result.returncode == 0:
+                    # Return base64 encoded content of file
+                    return
+                else:
+                    logger.error(f"Failed to generate quote:{result.stdout}")
+                    raise HTTPException(
+                        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                        detail=f"Failed to generate quote.",
+                    )
+        except HTTPException:
+            raise
+        except Exception as e:
+            logger.error(f"Unexpected error gathering GPU evidence:{e}")
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"Unexpected error gathering GPU evidence.",
+            )
+
+
+def run():
+    """Main entry point."""
+    try:
+        # Load configuration using Pydantic
+        config = ServerConfig()
+
+        # Setup logging level based on config
+        if config.debug:
+            logging.getLogger().setLevel(logging.DEBUG)
+            logger.debug("Debug mode enabled")
+            logger.debug("Configuration: %s", config.export_json())
+
+        # Validate required TLS configuration
+        if not config.tls_cert_path or not config.tls_key_path:
+            logger.warning("TLS certificates not configured, running in insecure mode")
+
+        # Create and run server
+        server = TdxQuoteServer(config)
+        server.run()
+
+    except Exception as e:
+        logger.exception("Failed to start admission controller: %s", e)
+        raise
+
+
+if __name__ == "__main__":
+    run()
